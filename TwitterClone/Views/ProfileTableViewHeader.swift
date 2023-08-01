6cfb945090bc82ccad6559f68cbc988fcfc5a511
Micah <micahburnside@protonamil.com>
2023-08-01 08:31:38 -0400

    adds profileHeaderImageView to ProfileTableViewHeader

--- TwitterClone/Views/ProfileTableViewHeader.swift
+++ TwitterClone/Views/ProfileTableViewHeader.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ProfileTableViewHeader: UIView {
 
+    private let profileHeaderImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "header")
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .red
+        addSubview(profileHeaderImageView)
+        
+        configureConstraints()
     }
-    
+    private func configureConstraints() {
+            let profileHeaderImageViewConstraints = [
+                profileHeaderImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+                profileHeaderImageView.topAnchor.constraint(equalTo: topAnchor),
+                profileHeaderImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+                profileHeaderImageView.heightAnchor.constraint(equalToConstant: 180)
+            ]
+        
+        NSLayoutConstraint.activate(profileHeaderImageViewConstraints)
+    }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
