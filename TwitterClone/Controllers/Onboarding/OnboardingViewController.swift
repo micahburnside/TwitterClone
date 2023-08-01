945ab1ca9faeeba0cdc2bae35a90b7ab05a45837
Micah <micahburnside@protonamil.com>
2023-08-01 12:54:16 -0400

    Adds RegisterViewController. Adds registerTitleLabel, EmailTextField, and PasswordTextField to RegisterViewController

--- TwitterClone/Controllers/Onboarding/OnboardingViewController.swift
+++ TwitterClone/Controllers/Onboarding/OnboardingViewController.swift
@@ -16,15 +16,58 @@ class OnboardingViewController: UIViewController {
         label.font = .systemFont(ofSize: 32, weight: .heavy)
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textAlignment = .center
+        label.textColor = .label
         return label
         
     }()
-
+    
+    private let createAccountButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Create Account", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 24, weight: .bold)
+        button.backgroundColor = UIColor(red: 29/255, green: 161/255, blue: 242/255, alpha: 1)
+        button.layer.masksToBounds = true
+        button.tintColor = .white
+        button.layer.cornerRadius = 30
+        return button
+    }()
+    
+    
+    private let promptLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.tintColor = .gray
+        label.text = "Have an account already?"
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        return label
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Login", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 14)
+        button.tintColor = UIColor(red: 29/255, green: 161/255, blue: 242/255, alpha: 1)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         view.addSubview(welcomeLabel)
+        view.addSubview(createAccountButton)
+        view.addSubview(promptLabel)
+        view.addSubview(loginButton)
+        
+        createAccountButton.addTarget(self, action: #selector(didTapCreateAccount), for: .touchUpInside)
+
         configureConstraints()
+    }
+    
+    @objc private func didTapCreateAccount() {
+        let vc = RegisterViewController()
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     private func configureConstraints() {
@@ -32,10 +75,32 @@ class OnboardingViewController: UIViewController {
             welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             welcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             welcomeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-
+        ]
+        
+        let createAccountButtonConstraints = [
+            createAccountButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            createAccountButton.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 20),
+            createAccountButton.widthAnchor.constraint(equalTo: welcomeLabel.widthAnchor, constant: -20),
+            createAccountButton.heightAnchor.constraint(equalToConstant: 60)
+        ]
+        
+        let promptLabelConstraints = [
+            promptLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            promptLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -60)
+        ]
+        
+        let loginButtonConstraints = [
+            loginButton.centerYAnchor.constraint(equalTo: promptLabel.centerYAnchor),
+            loginButton.leadingAnchor.constraint(equalTo: promptLabel.trailingAnchor, constant: 10)
         ]
         
         NSLayoutConstraint.activate(welcomeLabelConstraints)
+        NSLayoutConstraint.activate(createAccountButtonConstraints)
+        NSLayoutConstraint.activate(promptLabelConstraints)
+        NSLayoutConstraint.activate(loginButtonConstraints)
+
+
+
     }
 
 
---
+++ TwitterClone/Controllers/Onboarding/RegisterViewController.swift
@@ -0,0 +1,82 @@
+//
+//  RegisterViewController.swift
+//  TwitterClone
+//
+//  Created by Micah Burnside on 8/1/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    private let registerTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Create your account"
+        label.font = .systemFont(ofSize: 32, weight: .bold)
+        return label
+    }()
+    
+    private let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.attributedPlaceholder = NSAttributedString(
+            string: "Email",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray]
+        )
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.attributedPlaceholder = NSAttributedString(
+            string: "Password",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray]
+        )
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        view.addSubview(registerTitleLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        configureConstraints()
+    }
+    
+    private func configureConstraints() {
+        
+        let registerTitleLabelConstraints = [
+            registerTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            registerTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20)
+        ]
+        
+        let emailTextFieldConstraints = [
+            emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            emailTextField.topAnchor.constraint(equalTo: registerTitleLabel.bottomAnchor, constant: 20),
+            emailTextField.widthAnchor.constraint(equalToConstant: view.frame.width - 40),
+            emailTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emailTextField.heightAnchor.constraint(equalToConstant: 60)
+
+
+        ]
+        
+        let passwordTextFieldConstraints = [
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 15),
+            passwordTextField.widthAnchor.constraint(equalToConstant: view.frame.width - 40),
+            passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 60)
+        ]
+        
+        NSLayoutConstraint.activate(registerTitleLabelConstraints)
+        NSLayoutConstraint.activate(emailTextFieldConstraints)
+        NSLayoutConstraint.activate(passwordTextFieldConstraints)
+
+        
+    }
+
+}
