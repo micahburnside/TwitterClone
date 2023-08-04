7d5433e97b4c58b90eebd22046636deb83d7083b
Micah <micahburnside@protonamil.com>
2023-08-04 15:28:26 -0400

    adds TweetComposeViewController

---
+++ TwitterClone/Controllers/General/TweetComposeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TweetComposeViewController.swift
+//  TwitterClone
+//
+//  Created by Micah Burnside on 8/4/23.
+//
+
+import UIKit
+
+class TweetComposeViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+
+
+}
